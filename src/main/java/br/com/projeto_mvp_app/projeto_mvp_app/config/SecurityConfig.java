2601be40b697b4388854b8112570d62f6eb79ab8
Victor Hugo Negrisoli <victornegrisoli@xbrain.com.br>
2019-10-01 11:06:34 -0300

    Corrige bean BCrypt para produção

--- src/main/java/br/com/projeto_mvp_app/projeto_mvp_app/config/SecurityConfig.java
+++ src/main/java/br/com/projeto_mvp_app/projeto_mvp_app/config/SecurityConfig.java
@@ -13,6 +13,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+@SuppressWarnings("PMD.UnusedPrivateField")
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
