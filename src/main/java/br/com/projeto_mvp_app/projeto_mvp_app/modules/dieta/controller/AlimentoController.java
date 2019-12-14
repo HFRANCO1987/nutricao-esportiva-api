aa72cbd3f21462fe0da76d66e3ffe3df2ae573df
Victor Hugo Negrisoli <victorhugonegrisoli.ccs@hotmail.com>
2019-12-14 00:01:50 -0300

    Adiciona tabela de alimentos com base na tabela TACO

--- src/main/java/br/com/projeto_mvp_app/projeto_mvp_app/modules/dieta/controller/AlimentoController.java
+++ src/main/java/br/com/projeto_mvp_app/projeto_mvp_app/modules/dieta/controller/AlimentoController.java
@@ -1,7 +1,5 @@
 package br.com.projeto_mvp_app.projeto_mvp_app.modules.dieta.controller;
 
-import br.com.projeto_mvp_app.projeto_mvp_app.config.exception.ValidacaoException;
-import br.com.projeto_mvp_app.projeto_mvp_app.modules.dieta.dto.AlimentoResponse;
 import br.com.projeto_mvp_app.projeto_mvp_app.modules.dieta.model.Alimento;
 import br.com.projeto_mvp_app.projeto_mvp_app.modules.dieta.repository.AlimentoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,8 +8,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -22,17 +18,8 @@ public class AlimentoController {
     private AlimentoRepository alimentoRepository;
 
     @GetMapping("/{descricao}")
-    public List<AlimentoResponse> buscarPorDescricao(@PathVariable String descricao) {
-        var alimentoResponseList = new ArrayList<AlimentoResponse>();
-        alimentoRepository.findByDescricaoContainingIgnoreCase(descricao)
-            .forEach(alimento -> {
-                try {
-                    alimentoResponseList.add(AlimentoResponse.of(alimento));
-                } catch (IOException erro) {
-                    throw new ValidacaoException("Erro ao converter JSON: " + erro);
-                }
-            });
-        return alimentoResponseList;
+    public List<Alimento> buscarPorDescricao(@PathVariable String descricao) {
+        return alimentoRepository.findByDescricaoContainingIgnoreCase(descricao);
     }
 
     @GetMapping
--- src/main/java/br/com/projeto_mvp_app/projeto_mvp_app/modules/dieta/model/Alimento.java
+++ src/main/java/br/com/projeto_mvp_app/projeto_mvp_app/modules/dieta/model/Alimento.java
@@ -1,6 +1,5 @@
 package br.com.projeto_mvp_app.projeto_mvp_app.modules.dieta.model;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -19,21 +18,63 @@ public class Alimento {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Integer id;
-
     @ManyToOne
-    @JoinColumn(name = "FK_CATEGORIA", nullable = false)
+    @JoinColumn(name = "FK_CATEGORIA")
     private Categoria categoria;
-
-    @Column(name = "DESCRICAO", nullable = false, length = 100)
+    @Column
     private String descricao;
-
-    @Column(name = "QUANTIDADE_BASE", nullable = false)
-    private Integer quantidadeBase;
-
-    @Column(name = "UNIDADE_BASE", nullable = false, length = 10)
-    private String unidadeBase;
-
-    @Column(name = "ATRIBUTOS", nullable = false, length = 3000)
-    @JsonIgnore
-    private String atributos;
+    @Column
+    private Double umidade;
+    @Column
+    private Double energia_kcal;
+    @Column
+    private Double energia_kj;
+    @Column
+    private Double protei_g;
+    @Column
+    private Double lipideos_g;
+    @Column
+    private Double colesterol_mg;
+    @Column
+    private Double carboidrato_g;
+    @Column
+    private Double fibra_alimentar_g;
+    @Column
+    private Double cinzas_g;
+    @Column
+    private Double calcio_mg;
+    @Column
+    private Double magnesio_mg;
+    @Column
+    private Integer numero_alimento;
+    @Column
+    private Double manganes_mg;
+    @Column
+    private Double fosforo_mg;
+    @Column
+    private Double ferro_mg;
+    @Column
+    private Double sodio_mg;
+    @Column
+    private Double potassio_mg;
+    @Column
+    private Double cobre_mg;
+    @Column
+    private Double zinco_mg;
+    @Column
+    private Double retinol_mcg;
+    @Column
+    private Double re_mcg;
+    @Column
+    private Double era_mcg;
+    @Column
+    private Double tiami_mg;
+    @Column
+    private Double riboflavi_mg;
+    @Column
+    private Double piridoxi_mg;
+    @Column
+    private Double niaci_mg;
+    @Column
+    private Double vitami_c_mg;
 }
