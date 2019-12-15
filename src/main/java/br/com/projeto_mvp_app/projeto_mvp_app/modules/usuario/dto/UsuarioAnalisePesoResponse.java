03d623d4f44294e4ebf48d5e92a54c713a938912
Victor Hugo Negrisoli <victorhugonegrisoli.ccs@hotmail.com>
2019-12-14 23:48:46 -0300

    Corrige retorno de datas na mensagem de análise de peso do usuário

--- src/main/java/br/com/projeto_mvp_app/projeto_mvp_app/modules/usuario/dto/UsuarioAnalisePesoResponse.java
+++ src/main/java/br/com/projeto_mvp_app/projeto_mvp_app/modules/usuario/dto/UsuarioAnalisePesoResponse.java
@@ -1,13 +1,19 @@
 package br.com.projeto_mvp_app.projeto_mvp_app.modules.usuario.dto;
 
 import br.com.projeto_mvp_app.projeto_mvp_app.modules.usuario.model.PesoAltura;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
 import java.time.LocalDate;
 import java.time.Period;
+
+import static java.math.BigInteger.ZERO;
 
 @Data
 @Builder
@@ -18,23 +24,33 @@ public class UsuarioAnalisePesoResponse {
 
     private static final String DIA = " dia";
     private static final String DIAS = " dias";
-    private static final String SEMANA = " semana";
-    private static final String SEMANAS = " semanas";
     private static final String MES = " mês";
     private static final String MESES = " meses";
     private static final String ANO = " ano";
-    private static final String ANOS = " ano";
+    private static final String ANOS = " anos";
     private static final String PERDEU = "perdeu";
     private static final String AUMENTOU = " aumentou";
     private static final String MANTEVE = " manteve";
+    private static final Integer NUMERO_CASAS_DECIMAIS = 2;
+    private static DecimalFormat df = new DecimalFormat("0.00");
 
     private String mensagem;
-    private Double diferencaPeso;
-    private Double percentualPeso;
+    private Double pesoAtual;
+    @JsonFormat(pattern = "dd/MM/yyyy")
+    private LocalDate dataPesoAtual;
+    private Double pesoAnterior;
+    @JsonFormat(pattern = "dd/MM/yyyy")
+    private LocalDate dataPesoAnterior;
+    private BigDecimal diferencaPeso;
+    private BigDecimal percentualPeso;
     private String diferencaPeriodo;
 
     public static UsuarioAnalisePesoResponse of(String nome, PesoAltura atual, PesoAltura anterior) {
         var response = new UsuarioAnalisePesoResponse();
+        response.setPesoAtual(atual.getPeso());
+        response.setDataPesoAtual(atual.getDataCadastro().toLocalDate());
+        response.setPesoAnterior(anterior.getPeso());
+        response.setDataPesoAnterior(anterior.getDataCadastro().toLocalDate());
         response.setDiferencaPeriodo(getDiferencaPeriodo(atual.getDataCadastro().toLocalDate(),
             anterior.getDataCadastro().toLocalDate()));
         response.setDiferencaPeso(calcularDiferencaPeso(atual.getPeso(), anterior.getPeso()));
@@ -45,47 +61,47 @@ public class UsuarioAnalisePesoResponse {
     }
 
     private static String getDiferencaPeriodo(LocalDate atual, LocalDate anterior) {
-        if (Period.between(anterior, atual).getDays() == 1) {
-            return 1 + DIA;
+        var ano = Period.between(anterior, atual).getYears();
+        var mes = Period.between(anterior, atual).getMonths();
+        var dia = Period.between(anterior, atual).getDays();
+        if (ano > 1) {
+            return ano + ANOS;
         }
-        if (Period.between(anterior, atual).getDays() <= 6) {
-            return Period.between(anterior, atual).getDays() + DIAS;
-        }
-        if (Period.between(anterior, atual).getDays() == 7) {
-            return 1 + SEMANA;
-        }
-        if (Period.between(anterior, atual).getMonths() < 1) {
-            return Period.between(anterior, atual).getDays() / 7 + SEMANAS;
-        }
-        if (Period.between(anterior, atual).getMonths() == 1) {
-            return 1 + MES;
-        }
-        if (Period.between(anterior, atual).getMonths() < 12) {
-            return Period.between(anterior, atual).getMonths() + MESES;
-        }
-        if (Period.between(anterior, atual).getYears() == 1) {
+        if (ano == 1) {
             return 1 + ANO;
-        } else {
-            return Period.between(anterior, atual).getYears() + ANOS;
         }
+        if (ano == 0) {
+            if (mes <= 12 && mes > 1) {
+                return mes + MESES;
+            }
+            if (mes == 1) {
+                return 1 + MES;
+            }
+            if (mes == 0 && dia > 1) {
+                return dia + DIAS;
+            }
+        }
+        return 1 + DIA;
     }
 
     private static String getDiferencaPeso(Double pesoAtual, Double pesoAnterior) {
         return pesoAtual > pesoAnterior ? AUMENTOU : pesoAtual.equals(pesoAnterior) ? MANTEVE : PERDEU;
     }
 
-    private static Double calcularDiferencaPeso(Double pesoAtual, Double pesoAnterior) {
-        return Math.abs(pesoAtual - pesoAnterior);
+    private static BigDecimal calcularDiferencaPeso(Double pesoAtual, Double pesoAnterior) {
+        return new BigDecimal(Math.abs(pesoAtual - pesoAnterior)).setScale(NUMERO_CASAS_DECIMAIS, RoundingMode.HALF_UP);
     }
 
-    private static Double calcularPercentualPeso(Double pesoAtual, Double pesoAnterior) {
+    private static BigDecimal calcularPercentualPeso(Double pesoAtual, Double pesoAnterior) {
         if (pesoAtual < pesoAnterior) {
-            return Math.abs(100 - (pesoAtual / pesoAnterior) * 100);
+            return new BigDecimal(Math.abs(100 - (pesoAtual / pesoAnterior) * 100))
+                .setScale(NUMERO_CASAS_DECIMAIS, RoundingMode.HALF_UP);
         }
         if (pesoAtual > pesoAnterior) {
-            return Math.abs(100 - (pesoAnterior / pesoAtual) * 100);
+            return new BigDecimal(Math.abs(100 - (pesoAnterior / pesoAtual) * 100))
+                .setScale(NUMERO_CASAS_DECIMAIS, RoundingMode.HALF_UP);
         } else {
-            return 0.0;
+            return new BigDecimal(ZERO);
         }
     }
 
@@ -99,7 +115,7 @@ public class UsuarioAnalisePesoResponse {
                 + "kg (" + calcularPercentualPeso(pesoAtual, pesoAnterior) + "%) em " + diferencaPeriodo;
         }
         if (diferencaPeso.equals(AUMENTOU)) {
-            return "Olá, " + nome + ", você perdeu " + calcularDiferencaPeso(pesoAtual, pesoAnterior)
+            return "Olá, " + nome + ", você aumentou " + calcularDiferencaPeso(pesoAtual, pesoAnterior)
                 + "kg (" + calcularPercentualPeso(pesoAtual, pesoAnterior) + "%) em " + diferencaPeriodo;
         }
         return "Olá, " + nome + ", você manteve seu peso atual de " + pesoAtual + "kg";
