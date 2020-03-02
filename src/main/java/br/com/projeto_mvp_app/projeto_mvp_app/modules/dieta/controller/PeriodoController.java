f3c6a420eb29e9134926f976ceba1024d99d46fc
Victor Hugo Negrisoli <victornegrisoli@xbrain.com.br>
2020-03-02 17:38:23 -0300

    Adiciona endpoint para recuperar períodos de uma dieta

--- src/main/java/br/com/projeto_mvp_app/projeto_mvp_app/modules/dieta/controller/PeriodoController.java
+++ src/main/java/br/com/projeto_mvp_app/projeto_mvp_app/modules/dieta/controller/PeriodoController.java
@@ -2,9 +2,12 @@ package br.com.projeto_mvp_app.projeto_mvp_app.modules.dieta.controller;
 
 import br.com.projeto_mvp_app.projeto_mvp_app.modules.comum.response.SuccessResponseDetails;
 import br.com.projeto_mvp_app.projeto_mvp_app.modules.dieta.dto.periodo.PeriodoRequest;
+import br.com.projeto_mvp_app.projeto_mvp_app.modules.dieta.dto.periodo.PeriodoResponse;
 import br.com.projeto_mvp_app.projeto_mvp_app.modules.dieta.service.PeriodoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("api/periodos")
@@ -12,6 +15,11 @@ public class PeriodoController {
 
     @Autowired
     private PeriodoService periodoService;
+
+    @GetMapping("dieta/{dietaId}")
+    public List<PeriodoResponse> buscarPeriodosDaDieta(@PathVariable Integer dietaId) {
+        return periodoService.buscarPeriodosDaDieta(dietaId);
+    }
 
     @PostMapping
     public SuccessResponseDetails adicionarPeriodoDieta(@RequestBody PeriodoRequest request) {
--- src/main/java/br/com/projeto_mvp_app/projeto_mvp_app/modules/dieta/dto/periodo/PeriodoResponse.java
+++ src/main/java/br/com/projeto_mvp_app/projeto_mvp_app/modules/dieta/dto/periodo/PeriodoResponse.java
@@ -1,6 +1,7 @@
 package br.com.projeto_mvp_app.projeto_mvp_app.modules.dieta.dto.periodo;
 
 import br.com.projeto_mvp_app.projeto_mvp_app.modules.dieta.model.Periodo;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -16,6 +17,7 @@ public class PeriodoResponse {
 
     private Integer id;
     private String descricao;
+    @JsonFormat(pattern = "HH:mm")
     private LocalTime hora;
 
     public static PeriodoResponse of(Periodo periodo) {
